254275089235d0b6714c9f3f8aa72fd0319d9904
Naveen95-clt <naveenpm9@gmail.com>
2022-04-19 00:16:27 +0530

    19/04 one

---
+++ script/signup.js
@@ -0,0 +1,58 @@
+var nameError=document.getElementById("name-error");
+var emailError=document.getElementById("email-error");
+var passwordError=document.getElementById("password-error");
+var numberError=document.getElementById("number-error");
+
+
+function validateName(){
+    var thename=document.getElementById("name").value;
+    if(thename.length==0){
+      nameError.innerText="*Enter your name";
+      return false;
+    }
+   if(!thename.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+       nameError.innerText="Enter Full Name"
+    }
+    nameError.innerText=" ";
+    return true;
+}
+
+function validateNumber()
+{
+    var thenumber=document.getElementById('number').value;
+ if(thenumber.length==0){
+     numberError.innerText="*Please enter your number";
+     return false;
+ }
+ if(thenumber.length !==10){
+    numberError.innerText="*Please enter a 10 digit number";
+    return false;
+}
+ if(!thenumber.match(/^[0-9]{10}$/))
+    {numberError.innerText="*Enter a valid number";
+    return false;}
+ if(!thenumber.match(/\d{3}[-. ]?\d{3}[-. ]?\d{4}/))
+ {
+    numberError.innerText="*Enter a valid format";
+     return false;
+     
+ }   
+ else{ 
+     numberError.innerText="";
+     return true;}
+    
+}
+
+function validateEmail(){
+    var theemail=document.getElementById("email");
+    if(theemail.length==0){
+        emailError.innerText="*Please enter your email";
+        return false;}
+     if(!theemail.match(/^[A-Za-z0-9]+@[a-z0-9]+\.[a-z]{2,3}$/)) 
+    {emailError.innerText="Inavild Email";
+     return false;
+ }
+ emailError.innerText="";
+ return true;
+
+}
